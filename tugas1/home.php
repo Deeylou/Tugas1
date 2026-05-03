1d71c81c1b44f34dc039865919cc6dee3502b131
Deeylou <160565366+Deeylou@users.noreply.github.com>
2026-05-03 23:21:17 +0700

    Add files via upload

---
+++ tugas1/home.php
@@ -0,0 +1,20 @@
+<div class="row align-items-center">
+
+  <div class="col-md-4 text-center">
+    <img src="" class="rounded-circle">
+  </div>
+
+  <div class="col-md-8">
+    <h2 class="fw-bold">Your Name</h2>
+    <p class="text-muted">Mahasiswa Informatika</p>
+
+    <p>
+      Saya tertarik pada pengembangan web dan teknologi digital,
+      khususnya backend development menggunakan PHP.
+    </p>
+
+    <span class="badge bg-primary">Web Dev</span>
+    <span class="badge bg-success">Backend</span>
+  </div>
+
+</div>
