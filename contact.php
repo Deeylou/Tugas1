c378c9b629771292b8f09957b40799250b770291
Deeylou <160565366+Deeylou@users.noreply.github.com>
2026-05-03 23:33:04 +0700

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,27 @@
+<div class="card-group">
+
+  <div class="card text-center">
+    <div class="card-body">
+      <i class="bi bi-instagram"></i>
+      <h5>Instagram</h5>
+      <a href="#">@username</a>
+    </div>
+  </div>
+
+  <div class="card text-center">
+    <div class="card-body">
+      <i class="bi bi-whatsapp"></i>
+      <h5>WhatsApp</h5>
+      <a href="#">08xxxxxxxxxx</a>
+    </div>
+  </div>
+
+  <div class="card text-center">
+    <div class="card-body">
+      <i class="bi bi-envelope"></i>
+      <h5>Email</h5>
+      <a href="#">email@gmail.com</a>
+    </div>
+  </div>
+
+</div>
